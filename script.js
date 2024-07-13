8f6fbb71e8ff2bfae244bbbd391b2876971d7181
aaronsfunstuff <135081747+aaronsfunstuff@users.noreply.github.com>
2024-07-13 14:11:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const backToTopButton = document.getElementById('back-to-top');
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 300) {
+            backToTopButton.style.display = 'block';
+        } else {
+            backToTopButton.style.display = 'none';
+        }
+    });
+
+    backToTopButton.addEventListener('click', () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
     const events = document.querySelectorAll('.timeline-event');
     events.forEach(event => {
         event.addEventListener('click', () => {
             event.classList.toggle('active');
-            alert(event.querySelector('h2').innerText);
+            alert(event.querySelector('h3').innerText);
         });
     });
 });
